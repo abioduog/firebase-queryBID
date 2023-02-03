ee99380b7ed4adb14a3b68a39b45c573c1a97251
sankkakei <sankkakeii@gmail.com>
2023-02-03 16:08:05 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,116 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+
+const admin = require('firebase-admin');
+const key = require('./appgyver.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(key),
+});
+
+const fireDB = admin.firestore(); 
+
+const port = process.env.PORT || 5001;
+
+const app = express();
+
+// Use body-parser middleware to parse request bodies
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+// Set up a simple route
+app.get('/', (req, res) => {
+  res.send('Hello Appgyver World!');
+});
+
+// // create user
+// app.post('/create-user', (req, res) => {
+//   const { name, email, password } = req.body;
+//   fireDB.collection('users').add({
+//     name,
+//     email,
+//     password,
+//   })
+//     .then((doc) => {
+//       res.json({ message: `document ${doc.id} created successfully` });
+//     })
+//     .catch((err) => {
+//       res.status(500).json({ error: 'something went wrong' });
+//       console.error(err);
+//     });
+// });
+
+// get all users profile
+app.get('/users-profile', (req, res) => {
+  fireDB.collection('userProfile').get()
+    .then((snapshot) => {
+      const data = [];
+      snapshot.forEach((doc) => {
+        data.push(doc.data());
+      });
+      return res.json(data);
+    })
+    .catch((err) => console.log(err));
+});
+
+// get single userProfile by email or id
+app.get('/user-profile/:email', (req, res) => {
+  fireDB.collection('userProfile').where('email', '==', req.params.email).get()
+    .then((snapshot) => {
+      const data = [];
+      snapshot.forEach((doc) => {
+        data.push(doc.data());
+      });
+      return res.json(data);
+    })
+    .catch((err) => console.log(err));
+});
+
+
+
+// create event 
+app.post('/create-event', (req, res) => {
+  const {
+    date,
+    link,
+    title,
+    type,
+    creatorId,
+  } = req.body;
+  fireDB.collection('events').add({
+    date,
+    link,
+    title,
+    type,
+    creatorId,
+  })
+    .then((doc) => {
+      res.json({ message: `document ${doc.id} created successfully` });
+    })
+    .catch((err) => {
+      res.status(500).json({ error: 'something went wrong' });
+      console.error(err);
+    });
+});
+
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
+
+
+// module.exports = {
+//   "type": process.env.type,
+//   "project_id": process.env.project_id,
+//   "private_key_id": process.env.private_key_id,
+//   "private_key": process.env.private_key,
+//   "client_email": process.env.client_email,
+//   "client_id": process.env.client_id,
+//   "auth_uri": process.env.auth_uri,
+//   "token_uri": process.env.token_uri,
+//   "auth_provider_x509_cert_url": process.env.auth_provider_x509_cert_url,
+//   "client_x509_cert_url": process.env.client_x509_cert_url
+// }
